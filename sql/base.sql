767d8dd303252b104e90cc0ad104b2aef23de22c
jonathan0haurat <jonathan_haurat@yahoo.com>
2015-06-23 11:08:42 +0200

    SQL

--- sql/base.sql
+++ sql/base.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Mar 23 Juin 2015 à 10:39
+-- Généré le :  Mar 23 Juin 2015 à 11:08
 -- Version du serveur :  5.6.17
 -- Version de PHP :  5.5.12
 
@@ -30,8 +30,17 @@ CREATE TABLE IF NOT EXISTS `articles` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `Titre` varchar(535) NOT NULL,
   `article` varchar(535) NOT NULL,
+  `auteur` varchar(100) NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
+
+--
+-- Contenu de la table `articles`
+--
+
+INSERT INTO `articles` (`ID`, `Titre`, `article`, `auteur`) VALUES
+(4, 'test1', 'test de nouvel article', ''),
+(5, 'test2', 'test', 'blabla');
 
 -- --------------------------------------------------------
 
